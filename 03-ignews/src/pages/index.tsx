7ac3c4c4f1d9079daa0ed53f8adc64b68a8a00a7
Willian S. Praciano <willian.s.praciano@gmail.com>
2021-11-05 02:26:57 -0300

    Chapter III - Next.js Fundamentals - Application Structure

---
+++ 03-ignews/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <title>ig.news</title>
+      <h1>
+        Hello <span>World</span>
+      </h1>
+    </>
+  );
+}
