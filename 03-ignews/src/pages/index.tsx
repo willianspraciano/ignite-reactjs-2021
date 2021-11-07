9b6877f7f190c542605b6c1bfe1087c5d2dbdf36
Willian S. Praciano <willian.s.praciano@gmail.com>
2021-11-07 13:28:22 -0300

    Chapter III - Next.js fundamentals - Components and Pages: Header, SignInButton, Page home and SubscribeButton

--- 03-ignews/src/pages/index.tsx
+++ 03-ignews/src/pages/index.tsx
@@ -1,12 +1,28 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
-      <title>ig.news</title>
-      <h1>
-        Hello <span>World</span>
-      </h1>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span> 👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
