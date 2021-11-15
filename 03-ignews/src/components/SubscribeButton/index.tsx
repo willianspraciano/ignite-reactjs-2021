ea83205bc307288196b036f61d9e8b4e485f5e76
Willian S. Praciano <willian.s.praciano@gmail.com>
2021-11-15 12:35:35 -0300

    Chapter III - Next.js fundamentals - Integrating with API

--- 03-ignews/src/components/SubscribeButton/index.tsx
+++ 03-ignews/src/components/SubscribeButton/index.tsx
@@ -1,6 +1,10 @@
 import styles from './styles.module.scss';
 
-export function SubscribeButton() {
+interface subscribeButtonProps {
+  priceId: string;
+}
+
+export function SubscribeButton({ priceId }: subscribeButtonProps) {
   return (
     <button type="button" className={styles.subscribeButton}>
       Subscribe now
--- 03-ignews/src/pages/index.tsx
+++ 03-ignews/src/pages/index.tsx
@@ -1,9 +1,18 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -17,12 +26,34 @@ export default function Home() {
           </h1>
           <p>
             Get acess to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   );
 }
+
+//export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JuVKnK40QCJq7vAAkhLAND0', {
+    expand: ['product'], // para mostrar nome do produto e descrição
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USd',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
