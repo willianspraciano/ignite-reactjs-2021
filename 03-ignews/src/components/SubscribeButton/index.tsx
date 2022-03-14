1f0e62d98e333ca3518fd66143e24b1553c33ccb
Willian Praciano <willian.s.praciano@gmail.com>
2022-03-13 23:22:43 -0300

    adding last changes

--- 03-ignews/src/components/SubscribeButton/index.tsx
+++ 03-ignews/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { useSession, signIn } from 'next-auth/client';
 import styles from './styles.module.scss';
 
 interface subscribeButtonProps {
@@ -5,8 +6,23 @@ interface subscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: subscribeButtonProps) {
+  const [session] = useSession();
+  
+  function handleSubscribe(){
+    if(!session) { // verifica se usuário está logado
+      signIn('github');
+      return;
+    }
+
+    // criação da checkout session
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button 
+      type="button" 
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
