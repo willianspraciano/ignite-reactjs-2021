d566282f799fb85d61fa1bc69c2aef2f402f74f8
Willian S. Praciano <willian.s.praciano@gmail.com>
2023-03-20 17:04:42 -0300

    Chapter III - Back-end in the front-end - create component ActiveLink

--- 03-ignews/src/components/Header/index.tsx
+++ 03-ignews/src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { ActiveLink } from '../ActiveLink';
+
 import { SignInButton } from '../SignInButton';
 
 import styles from './styles.module.scss';
@@ -8,8 +10,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
