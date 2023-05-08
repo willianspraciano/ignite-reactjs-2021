7024cff620882eff8398af03c5054c67528b6e78
Willian S. Praciano <willian.s.praciano@gmail.com>
2023-05-08 10:48:52 -0300

    Chapter IV - responsive header

--- 04-dashgo/src/components/Header/Profile.tsx
+++ 04-dashgo/src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 
-export function Profile() {
+interface IProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: IProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Willian S. Praciano</Text>
-        <Text color="gray.300" fontSize="small">
-          willian.s.praciano@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Willian S. Praciano</Text>
+          <Text color="gray.300" fontSize="small">
+            willian.s.praciano@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size="md"
