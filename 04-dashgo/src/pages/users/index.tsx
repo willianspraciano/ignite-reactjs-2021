17b307f66755cdee1dc8207c01dc2d3459b39fec
Willian S. Praciano <willian.s.praciano@gmail.com>
2023-05-16 17:39:25 -0300

    Chapter IV - configuring axios

--- 04-dashgo/src/pages/users/index.tsx
+++ 04-dashgo/src/pages/users/index.tsx
@@ -24,13 +24,13 @@ import { Sidebar } from '@/components/Sidebar';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Pagination } from '@/components/Pagination';
 import { User } from '@/@types/User';
+import { api } from '@/services/api';
 
 export default function UserList() {
   const { data, isLoading, isFetching, error } = useQuery({
     queryKey: ['users'],
     queryFn: async () => {
-      const response = await fetch('http://localhost:3000/api/users');
-      const data = await response.json();
+      const { data } = await api.get('users');
 
       const users: User[] = data.users.map((user: User) => {
         return {
