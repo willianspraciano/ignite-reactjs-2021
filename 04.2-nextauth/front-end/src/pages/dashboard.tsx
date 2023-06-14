adbe7bc09d032459a3b1678827dacafa2e832889
Willian S. Praciano <willian.s.praciano@gmail.com>
2023-06-13 21:53:42 -0300

    Chapter IV - create permission hook

--- 04.2-nextauth/front-end/src/pages/dashboard.tsx
+++ 04.2-nextauth/front-end/src/pages/dashboard.tsx
@@ -4,15 +4,25 @@ import { AuthContext } from '@/contexts/AuthContext';
 import { setupAPIClient } from '@/services/api';
 import { api } from '@/services/apiClient';
 import { withSSRAuth } from '@/utils/withSSRAuth';
+import { useCan } from '@/hooks/useCan';
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
+
+  const userCanSeeMetrics = useCan({
+    permissions: ['metrics.list'],
+  });
 
   useEffect(() => {
     api.get('/me').then((response) => console.log(response));
   }, []);
 
-  return <h1>Dashboard: {user?.email}</h1>;
+  return (
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+      {userCanSeeMetrics && <div>Métricas</div>}
+    </>
+  );
 }
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
